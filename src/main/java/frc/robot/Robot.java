ea47a985993f52f1148a09badb4130c0ad490c6d
Calcium22 <strumbusfriend@gmail.com>
2026-02-21 17:01:40 -0600

    code to work with

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
 //revrobotics imports
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkFlex;
@@ -30,16 +32,23 @@ public class Robot extends TimedRobot {
   private final XboxController m_p1Controller = new XboxController(0);
   private final Timer m_timer = new Timer();
 
-  // Turret & Shooter
-  private final SparkMax m_turretMotor = new SparkMax(5, MotorType.kBrushed);
-  private final SparkClosedLoopController m_intakePID = m_turretMotor.getClosedLoopController();
+  // Turret & Shooter & hopper motors
+
+  private final SparkMax m_hopperMotor = new SparkMax(6, MotorType.kBrushless);
+private final SparkClosedLoopController m_hopperPID = m_hopperMotor.getClosedLoopController();
+private final RelativeEncoder m_hopperEncoder = m_hopperMotor.getEncoder();
+double hopper_retract = 0; 
+double hopper_extend = -0; 
+   private final SparkFlex m_intakeMotor = new SparkFlex(8, MotorType.kBrushless);
+  private final SparkClosedLoopController m_intakePID = m_intakeMotor.getClosedLoopController();
+  private final RelativeEncoder m_intakeEncoder = m_intakeMotor.getEncoder();
   
   private final SparkFlex m_shooterMotor = new SparkFlex(7, MotorType.kBrushless);
   private final SparkClosedLoopController m_shooterPID = m_shooterMotor.getClosedLoopController();
   private final RelativeEncoder m_shooterEncoder = m_shooterMotor.getEncoder();
 
   public static int positionIndex = 1;
-  public static double position[] = {2000, 3900, 5000, 6000}; // Preset shooter RPMs for different positions, array starts at 0 index
+  public static double position[] = {2000, 6000, 5000, 6000}; // Preset shooter RPMs for different positions, array starts at 0 index
   public static double kShooterRPM = position[positionIndex]; // Target shooter RPM
    public static int positionIndexPID = 1;
   public static double positionP[] = {1, 2, 3, 4}; // these are example values for PID tuning change them for actual
@@ -74,6 +83,14 @@ public class Robot extends TimedRobot {
     m_rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     // 2. Configure Shooter
+    SparkMaxConfig hopperConfig = new SparkMaxConfig();
+    
+    hopperConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .p(5).i(0.0).d(0.0).outputRange(-.030, .030);;
+
+    m_hopperPID.setReference(hopper_retract, ControlType.kPosition);
+
     SparkFlexConfig shooterConfig = new SparkFlexConfig();
     shooterConfig.closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
@@ -81,14 +98,16 @@ public class Robot extends TimedRobot {
         .outputRange(-1.0, 1.0);
       shooterConfig.closedLoopRampRate(0.5); // 0.5 seconds from neutral to full
 
-        SparkMaxConfig intakeConfig = new SparkMaxConfig();
+        SparkFlexConfig intakeConfig = new SparkFlexConfig();
     intakeConfig.closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         .p(0.00025).i(0.0).d(0.0)
-        .outputRange(-1.0, 1.0);
+        .outputRange(-1.0, 1.0).velocityFF(0.0003);
     
+    m_hopperMotor.configure(hopperConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     m_shooterMotor.configure(shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_turretMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
   }
 
   @Override
@@ -96,54 +115,60 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-    if (m_timer.get() < 2.0) {
-      m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor();
-    }
+  
   }
   public void teleopInit() {}
 
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_p1Controller.getLeftY() * 0.5, -m_p1Controller.getRightX() * 0.5);
+  m_robotDrive.arcadeDrive(-m_p1Controller.getLeftY() * 0.5, -m_p1Controller.getRightX() * 0.5);
 
+   
     if (m_p1Controller.getBackButtonPressed()) {
-      positionIndex++;
-      if (positionIndex > 3) {
-        positionIndex = 0;
-      }
-      kShooterRPM = position[positionIndex];
-    // Use setSetpoint instead of setReference
-    //intake mode
-    if (m_p1Controller.getYButton()) {
-        m_intakePID.setSetpoint(-6000, ControlType.kVelocity);
-        m_shooterPID.setSetpoint(kShooterRPM, ControlType.kVelocity);
+        positionIndex = (positionIndex + 1) % 4;
+        kShooterRPM = position[positionIndex];
     }
-    //shooting mode
+    // hopper to shooter
+    if (m_p1Controller.getYButton()) {
+        m_intakePID.setReference(6000, ControlType.kVelocity);
+        m_shooterPID.setReference(kShooterRPM, ControlType.kVelocity);
+    } 
+    // into hopper
     else if (m_p1Controller.getXButton()) { 
-        m_intakePID.setSetpoint(6000, ControlType.kVelocity);
-        m_shooterPID.setSetpoint(kShooterRPM, ControlType.kVelocity);
-    } else if (m_p1Controller.getAButton()) {
-      m_shooterPID.setSetpoint(kShooterRPM, ControlType.kVelocity);
-      m_intakePID.setSetpoint(0, ControlType.kVelocity);
+        m_intakePID.setReference(-6000, ControlType.kVelocity);
+        m_shooterPID.setReference(kShooterRPM, ControlType.kVelocity);
+    } 
+    // ground to shooter
+    else if (m_p1Controller.getAButton()) {
+        m_shooterPID.setReference(kShooterRPM, ControlType.kVelocity);
+        m_intakePID.setReference(0, ControlType.kVelocity);
+    }
+    // hopper to ground
+    else if (m_p1Controller.getBButton()) {
+        m_shooterPID.setReference(-6000, ControlType.kVelocity);
+        m_intakePID.setReference(-6000, ControlType.kVelocity);
     }
     else {
-        // Only stop if NO buttons are pressed
-        m_shooterPID.setSetpoint(0, ControlType.kVelocity);
-        m_intakePID.setSetpoint(0, ControlType.kVelocity);
-    }
-    
-    if (m_p1Controller.getLeftBumper()) {
-      position[1] -= 100;
-    }
-    if (m_p1Controller.getRightBumper()) {
-      position[1] += 100;
+        m_shooterPID.setReference(0, ControlType.kVelocity);
+        m_intakePID.setReference(0, ControlType.kVelocity);
     }
 
-    // Display current shooter RPM
+    // Hopper Logic
+
+    int pov = m_p1Controller.getPOV();
+    if (pov == 0) {
+        m_hopperPID.setReference(hopper_retract, ControlType.kPosition);
+    } else if (pov == 180) {
+        m_hopperPID.setReference(hopper_extend, ControlType.kPosition);
+    }
+
+    // Bumpers to tune RPM
+    if (m_p1Controller.getLeftBumperPressed()) { position[positionIndex] -= 100; }
+    if (m_p1Controller.getRightBumperPressed()) { position[positionIndex] += 100; }
+
+// Display current shooter RPM
     System.out.println("Shooter RPM: " + m_shooterEncoder.getVelocity());
   }
 }
-}
+
